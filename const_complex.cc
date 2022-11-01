2d68dd077099cc8168961d48f1ee5c77138c7a07
Julien Esseiva <jesseiva@lbl.gov>
2022-10-31 17:19:58 -0700

    update comments

--- const_complex.cc
+++ const_complex.cc
@@ -6,8 +6,8 @@ int main(int argc, char *argv[]) {
     const std::complex<float> cc{0.0, 0.0};
     std::complex<float> c{0.0, 0.0};
 
-    //this will fail to compile with nvc++ when c++ standard >= 14
-    //gcc, clang and nvcc can compile it since c++11
+    //this will fail to compile with nvc++ 22.9 when c++ standard >= 14
+    //gcc, clang and nvcc, nvc++ 22.7 can compile it since c++11
     std::cout << std::hypot(cc.real(), cc.imag()) << std::endl;
 
     //compiles
